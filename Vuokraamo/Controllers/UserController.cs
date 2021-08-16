deb726551c6f5345320287d7640aba9eacf17bfa
GitMatias <86785955+GitMatias@users.noreply.github.com>
2021-08-16 15:26:09 +0300

    Perkele

--- Vuokraamo/Controllers/UserController.cs
+++ Vuokraamo/Controllers/UserController.cs
@@ -1,15 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Vuokraamo.Models;
 
 namespace Vuokraamo.Controllers
 {
-    public class UserController : Controller
+    public class UserController : Controller 
     {
-        public IActionResult Index()
+  
+        private readonly ILogger<UserController> _logger;
+        private readonly VarastoDBContext _context;
+
+        public UserController(ILogger<UserController> logger, VarastoDBContext context)
         {
+            _logger = logger;
+            _context = context;
+        }
+
+
+        public IActionResult Index()    // Signup ja asiakkuuden hallinta
+        {
+            VarastoDBContext db = _context;
+
+
             return View();
         }
     }
