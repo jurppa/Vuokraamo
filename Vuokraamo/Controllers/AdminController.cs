b0288df483ce4c91cc2cfe0b962c79cabdde6907
jurppa <n0heju00@students.oamk.fi>
2021-08-17 13:12:22 +0300

    AdminController

---
+++ Vuokraamo/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vuokraamo.Models;
+
+namespace Vuokraamo.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            return View("Index")
+        }
+    }
+}
