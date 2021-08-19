1d244ca973c05c0e35e318370233fbe22018c316
Tblast <nguyen-thien@outlook.com>
2021-08-19 14:33:45 +0300

    UnitTest projekti lisätty

---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using Vuokraamo;
+
+namespace UnitTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void OnkoTuotetta()
+        {
+            Vuokraamo.Controllers.
+            Assert.Pass();
+        }
+    }
+}
